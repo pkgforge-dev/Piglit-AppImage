6c9c5df7d58652e67840e8a71f21f0a33c44219d
Link4Electronics <link4electronics@gmail.com>
2026-04-01 01:05:54 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,63 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cargo             \
+    cmake             \
+    glslang           \
+    meson             \
+    python-mako       \
+    python-numpy      \
+	libxslt			  \
+    glu               \
+    libcaca           \
+    libxkbcommon      \
+    ocl-icd           \
+    opencl-headers    \
+    vulkan-icd-loader \
+    vulkan-headers    \
+    wayland-protocols
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building waffle..."
+echo "---------------------------------------------------------------"
+REPO="https://gitlab.freedesktop.org/mesa/waffle"
+git clone --depth 1 "$REPO" ./waffle
 
-# If the application needs to be manually built that has to be done down here
+arch-meson ./waffle build --buildtype release -D build-manpages=false -D build-htmldocs=false -D build-examples=false
+meson compile -C build
+meson install -C build
+rm -rf ./build
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Building Vkrunner..."
+echo "---------------------------------------------------------------"
+REPO="https://gitlab.freedesktop.org/mesa/vkrunner"
+git clone --depth 1 "$REPO" ./vkrunner
+
+cd ./vkrunner
+export RUSTUP_TOOLCHAIN=stable
+export RUSTUP_TOOLCHAIN=stable
+export CARGO_TARGET_DIR=target
+cargo fetch --target host-tuple
+cargo build --offline --release --all-features
+mv -v target/release/vkrunner /usr/bin/vkrunner
+cd ../
+
+echo "Building Piglit..."
+echo "---------------------------------------------------------------"
+REPO="https://gitlab.freedesktop.org/mesa/piglit"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./piglit
+echo "$VERSION" > ~/version
+
+cmake -S ./piglit -B build \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+	-DPIGLIT_BUILD_CL_TESTS=1 \
+    -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j$(nproc)
+cmake --install build
+
