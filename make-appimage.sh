f7f707c3b2deabfebbbfcb6023a6e08289c14b42
Link4Electronics <link4electronics@gmail.com>
2026-04-01 01:09:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,13 +9,14 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DESKTOP=DUMMY
+export MAIN_BIN=piglit
 export DEPLOY_SYS_PYTHON=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/piglit /usr/lib/piglit /usr/lib/libpig*.so* /usr/lib/libhiz-util.so
 
 # Additional changes can be done in between here
 
