7efbef724d1dfde5bace293b44a04efad837a4f7
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:39:23 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q piglit-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q piglit-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -14,8 +14,6 @@ export DEPLOY_VULKAN=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/piglit /usr/lib/piglit
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
