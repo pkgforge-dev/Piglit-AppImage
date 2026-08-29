97ffd46b1a88cde38e32ea3417a145456a871577
Link4Electronics <link4electronics@gmail.com>
2026-08-29 10:57:37 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,15 +9,10 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_PYTHON=1
-export DEPLOY_OPENGL=1
-export DEPLOY_VULKAN=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/piglit /usr/lib/piglit
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --simple-test ./dist/*.AppImage
